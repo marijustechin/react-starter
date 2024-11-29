eb3f4755c531db9638f917dc42deef63e9ff58f2
D1JS <d1js@TECHIN.INTERNAL>
2024-11-29 14:49:17 +0400

    Pridėtas 404 komponentas

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,3 +1,7 @@
 export const Footer = () => {
-  return <footer className="text-center">Footer komponentas</footer>;
+  return (
+    <footer className="text-center ms-container border-t border-slate-400 mt-5 text-sm py-3">
+      Footer komponentas
+    </footer>
+  );
 };
--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,23 +1,33 @@
-import { NavLink } from 'react-router-dom';
-import { routerLinks } from '../../router/Router';
+import { Link, NavLink } from "react-router-dom";
+import { routerLinks } from "../../router/Router";
+import logo from "/assets/favicon-32x32.png";
 
 export const Header = () => {
   return (
-    <header className="flex justify-between">
-      <div>logo</div>
-      <div className="flex gap-2">
-        {routerLinks.map((link) => (
-          <div key={link.title}>
-            <NavLink
-              className={({ isActive }) =>
-                `${isActive ? 'bg-slate-400' : ''} rounded-lg py-1 px-2 border`
-              }
-              to={link.href}
-            >
-              {link.title}
-            </NavLink>
-          </div>
-        ))}
+    <header className="ms-container flex justify-between items-center border-b border-slate-400 py-3">
+      <div className="text-lg font-semibold min-w-fit">
+        <Link className="flex items-end" to={"/"}>
+          <img src={logo} alt="logo" />
+          arijus Techin
+        </Link>
+      </div>
+      <div className="w-full">
+        <div className="flex gap-2 justify-center">
+          {routerLinks.map((link) => (
+            <div key={link.title}>
+              <NavLink
+                className={({ isActive }) =>
+                  `${
+                    isActive ? "bg-slate-400" : ""
+                  } rounded-lg py-1 px-2 border`
+                }
+                to={link.href}
+              >
+                {link.title}
+              </NavLink>
+            </div>
+          ))}
+        </div>
       </div>
     </header>
   );
